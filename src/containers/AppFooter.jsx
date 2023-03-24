71738b669c66ca53c6b4dece2766aab3405d351a
ITchen <chenjian0521@126.com>
2023-03-24 15:16:01 +0800

    fix update 打包编译文件路径配置

--- src/containers/AppFooter.jsx
+++ src/containers/AppFooter.jsx
@@ -1,5 +1,5 @@
 import { Layout } from 'antd'
 
 const { Footer } = Layout
-const AppFooter = () => <Footer className='footer'>React Admin &copy;2019 Created By 游荡de蝌蚪</Footer>
+const AppFooter = () => <Footer className='footer'>React Admin &copy;2023 Created By ITchenjian</Footer>
 export default AppFooter
